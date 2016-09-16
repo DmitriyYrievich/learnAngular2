edc78b3a82f3f0e4395679093a121f8c9c42d74e
dmitriy <dmitriy@koderra.com>
2016-09-16 17:40:15 +0300

    16/09/16

--- quickstart/app/hero-detail.component.ts
+++ quickstart/app/hero-detail.component.ts
@@ -1,19 +1,33 @@
-import { Component, Input} from "@angular/core";
+// Keep the Input import for now, we'll remove it later:
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { HeroService } from './hero.service';
 import { Hero } from "./hero";
+
 
 @Component({
 	selector: 'hero-detail',
-	template: `
-			      <div *ngIf="hero">
-					<h2>hero detail {{hero.name}}</h2>
-					<h2>{{hero.id}}</h2>
-
-					<input type="text" [(ngModel)]="hero.name" />
-				  </div>
-			  `
+	templateUrl: 'app/hero-detail.component.html',
+	styleUrls: ["app/hero-detail.component.css"]
 })
 
-export class HeroDetailComponent{
-	@Input()
-	hero: Hero;
+export class HeroDetailComponent implements OnInit {
+
+	constructor(
+	  private heroService: HeroService,
+	  private route: ActivatedRoute) {
+	}
+
+	ngOnInit(): void {
+	  console.log(this.route.params);
+	  this.route.params.forEach((param: Params) => {
+	     let id = +param['id'];
+	     this.heroService.getHero(id).then(hero => this.hero = hero);
+	  });
+	}
+
+	goBack(): void{
+		window.history.back();
+	}
 }
---
+++ quickstart/app/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Hero } from "./hero";
+import { HeroService } from "./hero.service";
+
+@Component({
+    selector: 'my-heroes',
+    templateUrl: 'app/heroes.component.html',
+	styleUrls: ['app/heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+	
+	selectedHero: Hero;
+	heroes: Hero[];
+
+	constructor(private heroService: HeroService, private router: Router){}
+
+	ngOnInit(): void {
+		this.heroService.getHeroes().then( heroes => this.heroes = heroes);
+	}
+
+	selectHero(hero: Hero): void{
+		this.selectedHero = hero;
+	}
+
+	gotoDetail(){
+		this.router.navigate(["/detail", this.selectedHero.id]);
+	}
+}
+
+
