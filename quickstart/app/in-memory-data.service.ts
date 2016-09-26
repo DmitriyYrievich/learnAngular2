f3bfd18874e97c9a7673706914f54078878ad81b
dmitriy <dmitriy@koderra.com>
2016-09-26 15:07:11 +0300

    26/09/2016

--- quickstart/app/in-memory-data.service.ts
+++ quickstart/app/in-memory-data.service.ts
@@ -3,16 +3,16 @@ import { InMemoryDbService } from 'angular2-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     let heroes = [
-      {id: 11, name: 'Mr. Nice'},
-      {id: 12, name: 'Narco'},
-      {id: 13, name: 'Bombasto'},
-      {id: 14, name: 'Celeritas'},
-      {id: 15, name: 'Magneta'},
-      {id: 16, name: 'RubberMan'},
-      {id: 17, name: 'Dynama'},
-      {id: 18, name: 'Dr IQ'},
-      {id: 19, name: 'Magma'},
-      {id: 20, name: 'Tornado'}
+      {id: 11, name: 'Dimon@Campion'},
+      {id: 12, name: 'Vasua'},
+      {id: 13, name: 'Petya'},
+      {id: 14, name: 'Egorka'},
+      {id: 15, name: 'Kolyna'},
+      {id: 16, name: 'user_name'},
+      {id: 17, name: 'Четкий'},
+      {id: 18, name: 'Botan'},
+      {id: 19, name: 'anonim'},
+      {id: 20, name: 'Killer'}
     ];
     return {heroes};
   }
